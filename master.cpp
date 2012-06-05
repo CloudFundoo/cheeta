2050f7c60e1ce5f9f47bc0e25485921ad4ed3157
CloudFundoo <cloudfundoo@gmail.com>
2012-06-05 10:48:34 -0400

    Adding parts of app

--- master.cpp
+++ master.cpp
@@ -3,5 +3,11 @@
 int main(void)
 {
 	class server *pserver = new server();
-	
+	if(pserver->waitforclient())
+	{
+		pserver->recv();
+		pserver->send();
+	}
+
+	return 0;
 }
--- slave.cpp
+++ slave.cpp
@@ -3,5 +3,11 @@
 int main(void)
 {
 	class client *pclient = new client();
+	if(pclient->connect())
+	{
+		pclient->send();
+		pclient->recv();
+	}
+
 	return 0;
 }
