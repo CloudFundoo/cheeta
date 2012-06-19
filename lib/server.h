61bb452b41e3e906cde4e0d6d40cbfd2aba01c11
CloudFundoo <cloudfundoo@gmail.com>
2012-06-19 18:36:55 +0530

    Adding epoll based event loop in server

--- lib/server.h
+++ lib/server.h
@@ -6,10 +6,6 @@ class server{
 public:
         server();
         ~server();
-        int send(void *, unsigned int);
-        int recv(void *, unsigned int &);
-        int connect(void);
-	int waitforclient();
 private:
         int socketfd;
         int rsocketfd;
