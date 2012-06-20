3fcbd0fc4fc05f8c1c7cf5bb646a37a514e0584b
CloudFundoo <cloudfundoo@gmail.com>
2012-06-20 22:25:25 +0530

    Adding new  cheeta lib files

---
+++ lib/cheeta_ev.c
@@ -0,0 +1,60 @@
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/epoll.h>
+
+#include "cheeta_ev.h"
+
+struct cheeta_context *cheeta_event_init() 
+{
+	struct cheeta_context *cheeta = (struct cheeta_context *)
+				malloc(sizeof(struct cheeta_context));
+	cheeta->epfd = epoll_create(1024);	
+	
+	return cheeta;
+}
+
+unsigned int cheeta_event_get(struct cheeta_context *context, struct eventfd *eventbuffer, unsigned int buffersize) 
+{
+	int eventfdcount;
+	eventfdcount = epoll_wait(context->epfd, context->onevent, 32, 0);
+	if(eventfdcount > 0)
+	{	
+		int i = 0;
+		while(;i < eventfdcount; i++)
+		{
+			eventbuffer[i].fd = context->onevent[i].data.fd;
+			eventbuffer[i].out_event = context->onevent[i].events;
+		}		
+	}
+	return eventfdcount;
+}
+
+inline unsigned int cheeta_add_eventfd(struct cheeta_context *context, struct eventfd *event, unsigned int size)
+{	
+	struct epoll_event event2add;
+
+	event2add.data.fd = event->fd;
+	event2add.events = event->in_event;
+
+	return epoll_ctl(context->epfd, EPOLL_CTL_ADD, event2add.data.fd, &event2add);			
+}
+inline unsigned int cheeta_remove_eventfd(struct cheeta_context *context, structeventfd *event, unsigned int size)
+{
+	struct epoll_event event2remove;
+	
+	event2remove.data.fd = event->fd;
+	event2remove.events = 0;
+	
+	return epoll_ctl(context->epfd, EPOLL_CTL_DEL, event2remove.data.fd, &event2remove);
+}
+
+inline unsigned int cheeta_modify_eventfd(struct cheeta_context *context, structeventfd *event, unsigned int size)
+{
+	struct epoll_event event2remove;
+	
+	event2remove.data.fd = event->fd;
+	event2remove.events = event->in_event;
+	
+	return epoll_ctl(context->epfd, EPOLL_CTL_MOD, event2remove.data.fd, &event2remove);
+}
---
+++ lib/cheeta_ev.h
@@ -0,0 +1,33 @@
+struct eventfd{
+	int fd;
+	unsigned int in_event;
+	unsigned int out_event;	
+};
+
+struct cheeta_session{
+	int remotefd;
+	char buffer[4092];
+	int requestsize;
+	int responsesize;
+	int ready4write;
+	int writesize;
+};
+
+/** FD set **/
+struct cheeta_context {
+	int epfd;
+	struct epoll_event onevent[32];	
+	struct cheeta_session sessions[32];
+};
+
+
+#define CH_EV_READ	EPOLLIN
+#define CH_EV_WRITE	EPOLLOUT
+
+struct cheeta_context *cheeta_event_init(void);
+unsigned int cheeta_event_get(struct cheeta_context *, struct eventfd *, unsigned int);
+inline unsigned int cheeta_add_eventfd(struct cheeta_context *, struct eventfd *, unsigned int);
+
+inline unsigned int cheeta_remove_eventfd(struct cheeta_context *, struct eventfd *, unsigned int);
+
+inline unsigned int cheeta_modify_eventfd(struct cheeta_context *, struct eventfd *, unsigned int);
